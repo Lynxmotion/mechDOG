c83c04698058a79d4524ea819c5ce91d69fd78ae
Geraldine Barreto <geka_185@hotmail.com>
2022-11-25 18:28:06 -0400

    Added configuration file

---
+++ Arduino/mechDOG-IK-Gait/src/Config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+
+#define CONFIG_H
+
+#define UNDEFINED 0 //undefined (can also control the robot by API)
+#define C_RC 1 //(PPM)
+#define C_WIFI 2 //(Xbee)
+
+#define WSSID "Robotech"
+#define WPASS "deg.1812"
+
+#define QUADRUPED_CONTROL C_WIFI 
+
+#endif
